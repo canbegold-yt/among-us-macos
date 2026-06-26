eff84c65143c7b84cc8b1a0c7d9a82b96900a1bc
theodorexz <117528721+theodorexz@users.noreply.github.com>
2026-06-25 20:28:28 -0400

    Added new files & improved guide. Added Vercel Analytics to track view counts and such.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+import { Analytics } from "@vercel/analytics/next"
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
 })
 
 module.exports = withNextra()
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Among Us on MacOS</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/theodorexz/among-us-macos',
   },
   chat: {
     link: 'https://discord.com',
