7ce0b55d41ec2b2b69f1e2240b760837e32b94e6
theodorexz <117528721+theodorexz@users.noreply.github.com>
2026-06-26 21:28:16 -0400

    Edited _meta.global file format to follow Nextra documentation.

--- pages/_meta.global.js
+++ pages/_meta.global.js
@@ -1,33 +1,29 @@
-export default{
-  "index": {
-    "title": "👋 Introduction",
-    "position": 0,
-    "href": "/index"
+export default {
+  index: {
+    title: "👋 Introduction",
+    href: "/index"
   },
-  "dependencies": {
-    "title": "📦 Dependencies & Resources",
-    "sidebarTitle": "📦 Dependencies & Resources",
-    "position": 1,
-    "items": {
-      "rosetta2": "⚙️ Rosetta 2",
-      "homebrew": "🍺 Homebrew",
-      "Sikarugir": "✨ Sikarugir"
+  dependencies: {
+    title: "📦 Dependencies & Resources",
+    sidebarTitle: "📦 Dependencies & Resources",
+    items: {
+      rosetta2: "⚙️ Rosetta 2",
+      homebrew: "🍺 Homebrew",
+      Sikarugir: "✨ Sikarugir"
     }
   },
-  "guide": {
-    "title": "🚀 Quickstart Guide",
-    "position": 2,
-    "href": "/guide"
+  guide: {
+    title: "🚀 Quickstart Guide",
+    href: "/guide"
   },
   "faq-and-common-issues": {
-    "title": "❓ FAQ & Common Issues",
-    "position": 3,
-    "href": "/faq-and-common-issues"
+    title: "❓ FAQ & Common Issues",
+    href: "/faq-and-common-issues"
   },
-  "contact": {
-    "title": "Issues ↗",
-    "type": "page",
-    "href": "https://github.com/theodorexz/among-us-macos/issues",
-    "newWindow": true
+  contact: {
+    title: "Issues ↗",
+    type: "page",
+    href: "https://github.com/theodorexz/among-us-macos/issues",
+    newWindow: true
   }
 }
